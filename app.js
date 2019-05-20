aa15086ebbccf6ea0c4df5b098f64606355ee070
Stephen Spencer-Wong <b4dexample@gmail.com>
2019-05-20 14:40:32 -0700

    Drawing 10x10 squares
    Server communicates with client

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const app = express();
 var bodyParser = require('body-parser')
 var canvas = [];
 
+setInterval(drawData, 1500);
 
-
-app.get( '/', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile( path.join(__dirname, 'Draw.html'))
+
 
 })
 
@@ -18,18 +19,24 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json())
 
 app.post('/dimensions', function(req, res){ //gets dimensions of canvas
-  for(let i = 0; i < req.body.height; i++) {
+  for(let i = 0; i < req.body.height/10; i++) {
       canvas[i] = [];
-      for(let j = 0; j < req.body.width; j++) {
+      for(let j = 0; j < req.body.width/10; j++) {
           canvas[i][j] = "#ffffff";
       }
   }
-})
 
-app.post('/drawData', function(req, res){ //gets onclick data and sets color
-  canvas[req.body.x][req.body.y] = req.body.col
-  console.log(canvas[req.body.x][req.body.y])
 })
+function drawData(){
+  app.get('/drawData', function(req, res){ //gets onclick data and sets color
+    x = res.query.x/10
+    y = res.query.y/10
+    // canvas[Math.round(x/10)*10][Math.round(y/10)*10] = res.body.col //scales down the x and y to fit canvas which is 1/10 of the screen size with 10x10 pixels
+    // console.log(canvas[Math.round(x/10)*10][Math.round(y/10)*10])
+    console.log(req.query.y)
+
+  })
+}
 
 app.use(function(req, res, next) { //allowing data to be sent from both http://127.0.0.1:3000/ and http://localhost:3000/
   res.header("Access-Control-Allow-Origin", "*");

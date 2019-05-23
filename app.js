15197cea627dd467d5656d668bea929c60f9816c
Stephen Spencer-Wong <b4dexample@gmail.com>
2019-05-22 17:53:56 -0700

    Final project
    Server works and so does drawing

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const path = require('path')
-const http = require('http')
 const app = express();
 var bodyParser = require('body-parser')
 var canvas = [];
+var canvasTiles = [];
+var PORT = 3001;
 
 setInterval(drawData, 1500);
 
 app.get('/', function(req, res) {
   res.sendFile( path.join(__dirname, 'Draw.html'))
-
-
+  
 })
 
 app.use(bodyParser.urlencoded({
@@ -18,36 +18,40 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json())
 
-app.post('/dimensions', function(req, res){ //gets dimensions of canvas
-  for(let i = 0; i < req.body.height/10; i++) {
-      canvas[i] = [];
-      for(let j = 0; j < req.body.width/10; j++) {
-          canvas[i][j] = "#ffffff";
-      }
-  }
+// app.post('/dimensions', function(req, res){ //gets dimensions of canvas
 
-})
+
+// })
+
 function drawData(){
-  app.get('/drawData', function(req, res){ //gets onclick data and sets color
-    x = res.query.x/10
-    y = res.query.y/10
-    // canvas[Math.round(x/10)*10][Math.round(y/10)*10] = res.body.col //scales down the x and y to fit canvas which is 1/10 of the screen size with 10x10 pixels
-    // console.log(canvas[Math.round(x/10)*10][Math.round(y/10)*10])
-    console.log(req.query.y)
+  app.get('/drawData', function(req, res){ //gets onclick data and sets color and sends back the canvas
+    var x = parseInt(req.query.x)
+    var y = parseInt(req.query.y)
+    var color = req.query.col
+    // canvas[Math.round(x/10)*10][Math.round(y/10)*10] = color //scales down the x and y to fit canvas which is 1/10 of the screen size with 10x10 pixels
+
+    var newTile = {x: x, y: y, col: color}
+    canvasTiles.push(newTile)
+    
+    var dataCanv = canvasTiles
+    res.status(200).json(dataCanv)
+
 
   })
+
 }
 
-app.use(function(req, res, next) { //allowing data to be sent from both http://127.0.0.1:3000/ and http://localhost:3000/
+app.use(function(req, res, next) { //allowing data to be sent from both http://127.0.0.1:3001/ and http://localhost:3001/
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.listen(3000, function(err){
+
+app.listen(PORT, function(err){
   if(err){
     console.log('Error starting http server');
   } else {
-    console.log("Server running at http://127.0.0.1:3000/ or http://localhost:3000/");
+    console.log("Server running at http://127.0.0.1:" + PORT + "/ or http://localhost:"+PORT+"/");
   };
 });
